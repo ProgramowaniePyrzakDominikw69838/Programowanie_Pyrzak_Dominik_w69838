61152086f522e0c9d6ba715c8f4fe4725d8ffdd0
ProgramowaniePyrzakDominikw69838 <166232819+ProgramowaniePyrzakDominikw69838@users.noreply.github.com>
2024-06-01 21:10:34 +0200

    Add files via upload

--- lab3/script.js
+++ lab3/script.js
@@ -36,17 +36,68 @@
 // let S = Math.sqrt(p*(p-a)*(p-b)*(p-c));
 // console.log(S);
 
+
+// zad 7
+
 // Poproszenie użytkownika o podanie dwóch liczb całkowitych i przekształcenie ich na liczby
-let firstNumber = parseInt(prompt("Podaj pierwszą liczbę całkowitą:"));
-let secondNumber = parseInt(prompt("Podaj drugą liczbę całkowitą:"));
+// let firstNumber = parseInt(prompt("Podaj pierwszą liczbę całkowitą:"));
+// let secondNumber = parseInt(prompt("Podaj drugą liczbę całkowitą:"));
 
 // Sprawdzenie, czy podane wartości są liczbami
-if (!isNaN(firstNumber) && !isNaN(secondNumber)) {
+// if (!isNaN(firstNumber) && !isNaN(secondNumber)) {
     // Obliczenie sumy podanych liczb
-    let sum = firstNumber + secondNumber;
+//    let sum = firstNumber + secondNumber;
     // Wyświetlenie wyniku w oknie przeglądarki
-    alert("Wynik dodawania: " + sum);
-} else {
+//    alert("Wynik dodawania: " + sum);
+// } else {
     // Informacja w przypadku podania niepoprawnych wartości
-    alert("Podano niepoprawne wartości. Podaj liczby całkowite.");
+//    alert("Podano niepoprawne wartości. Podaj liczby całkowite.");
+// }
+
+// zad8 
+
+function findMax() {
+    // Pobierz wartości z elementów input
+    var num1 = parseFloat(document.getElementById("number1").value);
+    var num2 = parseFloat(document.getElementById("number2").value);
+    var num3 = parseFloat(document.getElementById("number3").value);
+
+    // Sprawdź, czy wszystkie wartości są liczbami
+    if (isNaN(num1) || isNaN(num2) || isNaN(num3)) {
+        console.log("Wprowadź prawidłowe liczby");
+        document.getElementById("result").innerText = "Wprowadź prawidłowe liczby";
+        return;
+    }
+
+    // Znajdź największą liczbę
+    var maxNum = Math.max(num1, num2, num3);
+
+    // Wyświetl wynik w konsoli
+    console.log("Największa liczba to: " + maxNum);
+
+    // Wyświetl wynik w elemencie HTML
+    document.getElementById("result").innerText = "Największa liczba to: " + maxNum;
+}
+
+function findGCD() {
+    let num1 = parseInt(document.getElementById('number1').value);
+    let num2 = parseInt(document.getElementById('number2').value);
+
+    if (isNaN(num1) || isNaN(num2)) {
+        document.getElementById('result').innerText = 'Proszę wprowadzić dwie liczby całkowite.';
+        return;
+    }
+
+    let gcd = euclideanAlgorithm(num1, num2);
+    document.getElementById('result').innerText = 'Największy wspólny dzielnik: ' + gcd;
+    console.log('Największy wspólny dzielnik:', gcd);
+}
+
+function euclideanAlgorithm(a, b) {
+    while (b !== 0) {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
 }
